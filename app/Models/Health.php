03cec0ec049eef475fcdbc8cd9f0e4f971a32a0a
TienHoangAnh <131014271+TienHoangAnh@users.noreply.github.com>
2023-06-12 16:45:54 +0700

    Create Model Person, Health
    Create Model, View, Controller of Person
    Create Model of Health
    Create migration: people and healths
    View: layout and person, Login, Register, index.

---
+++ app/Models/Health.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Health extends Model
+{
+    use HasFactory;
+    protected $table = 'health';
+    protected $fillable = [
+        'weight',
+        'height',
+        'symptom'
+    ];
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/login', AuthenticationController::class . '@loginIndex');
+Route::get('/register', AuthenticationController::class . '@registerIndex');
+Route::post('/login', AuthenticationController::class . '@login');
+Route::post('/register', AuthenticationController::class . '@register');
+Route::get('/logout', AuthenticationController::class . '@logout');
