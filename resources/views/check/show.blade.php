6ad4ff1f9275d9f64bb7bd8faef43d443196f09b
datpqgch211292 <datpqgch211292@fpt.edu.vn>
2023-06-15 13:00:15 +0700

    fix

--- resources/views/check/show.blade.php
+++ resources/views/check/show.blade.php
@@ -8,8 +8,10 @@
 <div class="card">
   <div class="card-body">
       <h4 class="card-title">{{$check->date}}</h4>    
-      <p class="card-text">{{$book->address}}</p>
-      <p class="card-text">{{$book->department}}</p>
+      <p class="card-text">{{$check->address}}</p>
+      <p class="card-text">{{$check->department}}</p>
       <h6 class="card-subtitle mb-2 text-muted">{{$check->doctor->name}}</h6>
   </div>
 </div>
+
+@endsection
