d741a985eafcd6ee4c33a2c1253c62c59c4cc2b7
khaccao17 <133207585+khaccao17@users.noreply.github.com>
2023-06-17 15:06:41 +0700

    Fix navbar

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,105 +1,91 @@
-<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-<div class="collapse" id="navbarToggleExternalContent">
-    <div class="bg-dark p-4">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                People
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="nav-link" href="{{url("/people/create")}}">Create new person <span class="visually-hidden"></span></a>
-                                <a class="nav-link" href="{{url("/people")}}">People list<span class="visually-hidden"></span></a>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Health
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="nav-link" href="{{url("/healths/create")}}">Create health information <span class="visually-hidden"></span></a>
-                                <a class="nav-link" href="{{url("/healths")}}">Health list<span class="visually-hidden"></span></a>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Doctor
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="nav-link" href="{{url("/doctors/create")}}">Create new doctor <span class="visually-hidden"></span></a>
-                                <a class="nav-link" href="{{url("/doctors")}}">Doctor list<span class="visually-hidden"></span></a>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Prescription
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="nav-link" href="{{url("/prescriptions/create")}}">Create new prescription <span class="visually-hidden"></span></a>
-                                <a class="nav-link" href="{{url("/prescriptions")}}">Prescription list<span class="visually-hidden"></span></a>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                MedicalCheck
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="nav-link" href="{{url("/checks/create")}}">Create MedicalCheck <span class="visually-hidden"></span></a>
-                                <a class="nav-link" href="{{url("/checks")}}">MedicalCheck list<span class="visually-hidden"></span></a>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
-</div>
-<nav class="navbar navbar-dark bg-dark">
-    <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            Category
-        </button>
-        <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-        <div>
-            <ul class="navbar-nav ml-auto">
-                @if (Auth::check())
-                <li class="nav-item">
-                    <a class="nav-link" href="#">{{ Auth::user()->name }} <span class="visually-hidden"></span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/logout">Logout <span class="visually-hidden"></span></a>
-                </li>
-                @else
-                <li class="nav-item">
-                    <a class="nav-link" href="/login">Login<span class="visually-hidden"></span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/register">Register<span class="visually-hidden"></span></a>
-                </li>
-                @endif
-            </ul>
-        </div>
-    </div>
-</nav>
+ <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ 
+ <nav style="background-color:black;" class="navbar navbar-expand-lg navbar-dark bg-dark">
+   <div class="container-fluid">
+     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent"
+       aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+     </button>
+     <div>
+         <img style="height: 50px; width:110px; border-radius:50px" src="/upload/ctd.png" alt="ctd">
+     </div>
+     <div style="margin-left: 100px; font-size:20px" class="collapse navbar-collapse" id="navbarToggleExternalContent">
+       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+         <li class="nav-item dropdown">
+           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+             aria-expanded="false">
+             People
+           </a>
+           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+             <li><a class="dropdown-item" href="{{url("/people/create")}}">Create new person</a></li>
+             <li><a class="dropdown-item" href="{{url("/people")}}">People list</a></li>
+           </ul>
+         </li>
+         <li class="nav-item dropdown">
+           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+             aria-expanded="false">
+             Health
+           </a>
+           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+             <li><a class="dropdown-item" href="{{url("/healths/create")}}">Create health information</a></li>
+             <li><a class="dropdown-item" href="{{url("/healths")}}">Health list</a></li>
+           </ul>
+         </li>
+         <li class="nav-item dropdown">
+           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+             aria-expanded="false">
+             Doctor
+           </a>
+           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+             <li><a class="dropdown-item" href="{{url("/doctors/create")}}">Create new doctor</a></li>
+             <li><a class="dropdown-item" href="{{url("/doctors")}}">Doctor list</a></li>
+           </ul>
+         </li>
+         <li class="nav-item dropdown">
+           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+             aria-expanded="false">
+             Prescription
+           </a>
+           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+             <li><a class="dropdown-item" href="{{url("/prescriptions/create")}}">Create new prescription</a></li>
+             <li><a class="dropdown-item" href="{{url("/prescriptions")}}">Prescription list</a></li>
+           </ul>
+         </li>
+         <li class="nav-item dropdown">
+           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+             aria-expanded="false">
+             MedicalCheck
+           </a>
+           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+             <li><a class="dropdown-item" href="{{url("/checks/create")}}">Create MedicalCheck</a></li>
+             <li><a class="dropdown-item" href="{{url("/checks")}}">MedicalCheck list</a></li>
+           </ul>
+         </li>
+       </ul>
+     </div>
+     <div>
+       <ul class="navbar-nav ml-auto" style="font-size:15px">
+         @if (Auth::check())
+         <li class="nav-item">
+           <a class="nav-link" href="#">{{ Auth::user()->name }}</a>
+         </li>
+         <li class="nav-item">
+           <a class="nav-link" href="/logout">Logout</a>
+         </li>
+         @else
+         <li class="nav-item">
+           <a class="nav-link" href="/login">Login</a>
+         </li>
+         <li class="nav-item">
+           <a class="nav-link" href="/register">Register</a>
+         </li>
+         @endif
+       </ul>
+     </div>
+   </div>
+ </nav>
+ 
